97fd50a8420eed81e9a4bce2e6d00430f23b7f06
M1julien <M1julien@student.bridgew.edu>
2020-09-08 12:18:00 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+age = 5
+dog_years = 7 * age
+print(f"The dog is {dog_years}")
+fake_years = "123"
+print(f"fake years is {fake_years}")
+fake_years_2 = fake_years * 7
+print(f"fake years 2 is {fake_years_2}")
